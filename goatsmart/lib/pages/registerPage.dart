742a4283ab3c22e16a8dd10d1c1c4a67e760f345
Gabriel Nassar <g.nassar@uniandes.edu.co>
2024-03-24 18:42:44 -0500

    In progress_auth

---
+++ goatsmart/lib/pages/registerPage.dart
@@ -0,0 +1,141 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:form_builder_validators/form_builder_validators.dart';
+import 'package:goatsmart/pages/login.dart';
+import 'package:goatsmart/utils/auth.dart';
+
+class RegisterPage extends StatefulWidget{
+  static const String routename = 'register';
+  const RegisterPage({super.key});
+  @override
+  State<RegisterPage> createState()=> _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  final GlobalKey<FormBuilderState> _formKey = GlobalKey<FormBuilderState>();
+
+  final AuthService _auth = AuthService();
+
+
+@override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 63, 85, 173),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: FormBuilder(
+            key: _formKey,
+            child: Container(
+              width: double.infinity,
+              height: size.height,
+              color : Color.fromARGB(255, 63, 85, 173),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Image.asset('assets/images/logo.png', scale: 4),
+                  const Center(child: Text('Register', style: TextStyle(color: Colors.white, fontSize: 30),),),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: FormBuilderTextField(
+                      autovalidateMode: AutovalidateMode.onUserInteraction,
+                      name: 'user',
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              color: Colors.blue.shade100),
+                            borderRadius: BorderRadius.circular(50)),
+                          prefixIcon: Icon(
+                            Icons.account_circle_outlined,
+                            color: Colors.blue.shade100,
+                          ),
+                          labelText: 'Usuario',
+                          labelStyle: const TextStyle(fontSize: 13),
+                          hintText: 'ejemplo@gmail.com'),
+                      keyboardType: TextInputType.emailAddress,
+                      validator: FormBuilderValidators.compose([
+                        FormBuilderValidators.required(errorText: 'Usuario requerido'),
+                        FormBuilderValidators.email(
+                          errorText: 'Debe ingresar un Correo valido')
+                      ]),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: FormBuilderTextField(
+                      autovalidateMode: AutovalidateMode.onUserInteraction,
+                      name: 'pass',
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.blue.shade100,),
+                          borderRadius: BorderRadius.circular(50)),
+                        prefixIcon: Icon(
+                          Icons.account_circle_outlined,
+                          color: Colors.black,
+                        ),
+                        labelText: 'Contraseña',
+                        labelStyle: const TextStyle(fontSize: 13)     
+                        ),
+                    keyboardType: TextInputType.emailAddress,                     
+                    validator: FormBuilderValidators.compose([
+                      FormBuilderValidators.required(
+                        errorText: 'Debe ingresar una contraseña')
+                    ]),
+                  ),
+                ),                                 
+                botonLogin(context),
+                SizedBox(height: size.height*0.1,),
+                GestureDetector(
+                  onTap: () => Navigator.popAndPushNamed(context, LoginPage.routeName),
+                  child: Text('¿Tienes cuenta?', style: TextStyle(color: Colors.blue.
+                  shade100),),
+                  )                
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  ElevatedButton botonLogin (BuildContext context){
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        primary: Colors.blue,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(50),
+        ),
+        padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 10),
+      ),
+      onPressed: () async {
+        if (_formKey.currentState?.validate()==true){
+          var result = await _auth.createAccount(
+            _formKey.currentState?.fields['user']?.value,
+            _formKey.currentState?.fields['pass']?.value,
+          );
+
+          if (result == 1){
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(content: Text('La contraseña es muy debil')));
+          } else if (result == 2){
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(content: Text('El correo ya esta en uso')));
+          } else if (result == 3){
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(content: Text('El correo no es valido')));
+          } else {
+            Navigator.popAndPushNamed(context, LoginPage.routeName);
+          }
+
+        } else {
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(content: Text('Por favor, ingrese los datos')));
+        }
+      },
+      child: const Text('Register', style: TextStyle(fontSize: 20),),
+    );
+  }
+}
