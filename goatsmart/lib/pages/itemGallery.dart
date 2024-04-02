fcc73b9d512e173b79fd6528fd3a287bb942c5cc
Gabriel Nassar <g.nassar@uniandes.edu.co>
2024-04-02 10:56:23 -0500

    Gallery Updated

--- goatsmart/lib/pages/itemGallery.dart
+++ goatsmart/lib/pages/itemGallery.dart
@@ -15,8 +15,19 @@ class _ItemGallery extends State<ItemGallery> {
       appBar: AppBar(
         title: const Text('Item Gallery'),
       ),
-      body: const Center(
-        child: Text('Item Gallery'),
+      body: ListView(
+        children: <Widget>[
+          Image.asset('assets/images/1.jpg'),
+          Image.asset('assets/images/2.jpg'),
+          Image.asset('assets/images/3.jpg'),
+          Image.asset('assets/images/4.jpg'),
+          Image.asset('assets/images/5.jpg'),
+          Image.asset('assets/images/6.jpg'),
+          Image.asset('assets/images/7.jpg'),
+          Image.asset('assets/images/8.jpg'),
+          Image.asset('assets/images/9.jpg'),
+          Image.asset('assets/images/10.jpg')          
+        ],
       ),
     );
   }
