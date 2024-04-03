ed329363297f37f6ae0f52227b2cc9777baa2198
Gabriel Nassar <g.nassar@uniandes.edu.co>
2024-04-03 16:44:11 -0500

    ItemGalleryUpdated

--- goatsmart/lib/pages/itemGallery.dart
+++ goatsmart/lib/pages/itemGallery.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
+import 'package:goatsmart/pages/home.dart';
 
 class ItemGallery extends StatefulWidget {
   static const String routeName = 'ItemGallery';
@@ -13,22 +16,84 @@ class _ItemGallery extends State<ItemGallery> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Item Gallery'),
+        //App bar size
+        toolbarHeight: 120.0,
+        leadingWidth: 120,
+        leading: Padding(
+          padding: const EdgeInsets.only(left: 15.0),
+          child: CircleAvatar(
+            // Face Circle
+            radius: 100.0,
+            backgroundImage: Image.asset('assets/images/prof.jpg').image,
+          ),
+        ),     
+        title: Row(
+          children: [
+            Expanded(
+              child: TextField(
+                decoration: InputDecoration(
+                  labelText: "Search",
+                  hintText: "Search",
+                  prefixIcon: Icon(Icons.search),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(25.0)),
+                  ),
+                ),
+              ),
+            ),      
+          ],            
+        ),
       ),
-      body: ListView(
-        children: <Widget>[
-          Image.asset('assets/images/1.jpg'),
-          Image.asset('assets/images/2.jpg'),
-          Image.asset('assets/images/3.jpg'),
-          Image.asset('assets/images/4.jpg'),
-          Image.asset('assets/images/5.jpg'),
-          Image.asset('assets/images/6.jpg'),
-          Image.asset('assets/images/7.jpg'),
-          Image.asset('assets/images/8.jpg'),
-          Image.asset('assets/images/9.jpg'),
-          Image.asset('assets/images/10.jpg')          
-        ],
-      ),
+      body: const Grid(),
     );
   }
 }
+
+class Grid extends StatelessWidget {
+  const Grid({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.count(
+      primary: false,
+      padding: const EdgeInsets.all(10),
+      crossAxisSpacing: 10,
+      mainAxisSpacing: 10,
+      crossAxisCount: 3,
+      children: <Widget>[
+        Container(
+          padding: const EdgeInsets.all(8),
+          color: Colors.teal[100],
+          child: const Text("He'd have you all unravel at the"),
+        ),
+        Container(
+          padding: const EdgeInsets.all(8),
+          color: Colors.teal[200],
+          child: const Text('Heed not the rabble'),
+        ),
+        Container(
+          padding: const EdgeInsets.all(8),
+          color: Colors.teal[300],
+          child: const Text('Sound of screams but the'),
+        ),
+        Container(
+          padding: const EdgeInsets.all(8),
+          color: Colors.teal[400],
+          child: const Text('Who scream'),
+        ),
+        Container(
+          padding: const EdgeInsets.all(8),
+          color: Colors.teal[500],
+          child: const Text('Revolution is coming...'),
+        ),
+        Container(
+          padding: const EdgeInsets.all(8),
+          color: Colors.teal[600],
+          child: const Text('Revolution, they...'),
+        ),
+      ],
+    );
+  }
+}    
