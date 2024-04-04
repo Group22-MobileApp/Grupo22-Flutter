0e9759ed795eb0a1e6b3cb0438caafa70703c92f
Gabriel Nassar <g.nassar@uniandes.edu.co>
2024-04-03 19:58:03 -0500

    Update itemGallery.dart

--- goatsmart/lib/pages/itemGallery.dart
+++ goatsmart/lib/pages/itemGallery.dart
@@ -15,6 +15,7 @@ class _ItemGallery extends State<ItemGallery> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         //App bar size
         toolbarHeight: 120.0,
@@ -32,11 +33,13 @@ class _ItemGallery extends State<ItemGallery> {
             Expanded(
               child: TextField(
                 decoration: InputDecoration(
+                  fillColor: Color.fromARGB(255, 211, 210, 210),
+                  filled: true,
                   labelText: "Search",
                   hintText: "Search",
                   prefixIcon: Icon(Icons.search),
                   border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(25.0)),
+                    borderRadius: BorderRadius.all(Radius.circular(25.0)),                    
                   ),
                 ),
               ),
@@ -44,7 +47,66 @@ class _ItemGallery extends State<ItemGallery> {
           ],            
         ),
       ),
-      body: const Grid(),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start, 
+        children: [
+          Padding( 
+            padding: const EdgeInsets.all(8.0), 
+            child: Text(
+              'Hello, Adriana!',
+              style: TextStyle(fontSize: 54, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween, 
+            children: [ 
+              Padding( 
+                //Padding left
+                padding: const EdgeInsets.only(left: 8.0),
+                child: Text(
+                  'Just For You',
+                  style: TextStyle(fontSize: 44),                                    
+                ),
+              ),
+              Padding( 
+                padding: const EdgeInsets.all(8.0), 
+                child: TextButton(
+                  onPressed: () => Navigator.popAndPushNamed(context, HomePage.routeName),
+                  style: TextButton.styleFrom(
+                    foregroundColor: Color.fromARGB(255, 0, 0, 0),
+                  ),
+                  child: const Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        'See All',
+                        style: TextStyle(
+                          fontSize: 38,
+                          // fontFamily: 'R',
+                          //Bold
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),                  
+                      const Padding(
+                        padding: EdgeInsets.only(left: 30),
+                      ),
+                      FloatingActionButton(onPressed: null, 
+                      backgroundColor: Color.fromARGB(230, 255, 168, 6), 
+                      foregroundColor: Colors.white,
+                      shape: CircleBorder(),
+                        child: Icon(Icons.arrow_forward_outlined),
+                      ),
+                    ],
+                  ),
+                ),  
+              ),
+            ],
+          ),
+          Expanded(
+            child: const Grid(),
+          ),
+        ],
+      ),
     );
   }
 }
@@ -61,37 +123,57 @@ class Grid extends StatelessWidget {
       padding: const EdgeInsets.all(10),
       crossAxisSpacing: 10,
       mainAxisSpacing: 10,
-      crossAxisCount: 3,
+      crossAxisCount: 2,
       children: <Widget>[
         Container(
           padding: const EdgeInsets.all(8),
-          color: Colors.teal[100],
-          child: const Text("He'd have you all unravel at the"),
+          color: const Color(0xFFE0F7FA),
+          child: Image.asset('assets/images/1.jpg', fit: BoxFit.fill),
         ),
         Container(
           padding: const EdgeInsets.all(8),
-          color: Colors.teal[200],
-          child: const Text('Heed not the rabble'),
+          color: const Color(0xFFE0F7FA),
+          child: Image.asset('assets/images/2.jpg', fit: BoxFit.fill),
         ),
         Container(
           padding: const EdgeInsets.all(8),
-          color: Colors.teal[300],
-          child: const Text('Sound of screams but the'),
+          color: const Color(0xFFE0F7FA),
+          child: Image.asset('assets/images/3.jpg', fit: BoxFit.fill),
         ),
         Container(
           padding: const EdgeInsets.all(8),
-          color: Colors.teal[400],
-          child: const Text('Who scream'),
+          color: const Color(0xFFE0F7FA),
+          child: Image.asset('assets/images/4.jpg', fit: BoxFit.fill),
         ),
         Container(
           padding: const EdgeInsets.all(8),
-          color: Colors.teal[500],
-          child: const Text('Revolution is coming...'),
+          color: const Color(0xFFE0F7FA),
+          child: Image.asset('assets/images/5.jpg', fit: BoxFit.fill),
         ),
         Container(
           padding: const EdgeInsets.all(8),
-          color: Colors.teal[600],
-          child: const Text('Revolution, they...'),
+          color: const Color(0xFFE0F7FA),
+          child: Image.asset('assets/images/6.jpg', fit: BoxFit.fill),
+        ),
+        Container(
+          padding: const EdgeInsets.all(8),
+          color: const Color(0xFFE0F7FA),
+          child: Image.asset('assets/images/7.jpg', fit: BoxFit.fill),
+        ),
+        Container(
+          padding: const EdgeInsets.all(8),
+          color: const Color(0xFFE0F7FA),
+          child: Image.asset('assets/images/8.jpg', fit: BoxFit.fill),
+        ),
+        Container(
+          padding: const EdgeInsets.all(8),
+          color: const Color(0xFFE0F7FA),
+          child: Image.asset('assets/images/9.jpg', fit: BoxFit.fill),
+        ),
+        Container(
+          padding: const EdgeInsets.all(8),
+          color: const Color(0xFFE0F7FA),
+          child: Image.asset('assets/images/10.jpg', fit: BoxFit.fill),
         ),
       ],
     );
