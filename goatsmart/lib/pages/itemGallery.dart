7085f9a3176a4ec1b4f324d2d443c545ec2e7cbb
Gabriel Nassar <g.nassar@uniandes.edu.co>
2024-05-27 02:48:16 -0500

    Update itemGallery.dart

--- goatsmart/lib/pages/itemGallery.dart
+++ goatsmart/lib/pages/itemGallery.dart
@@ -688,108 +688,121 @@ class _ItemGallery extends State<ItemGallery> {
         .format(double.parse(formattedPrice));
   }
 
-  Future<void> _showItemDialog(BuildContext context, MaterialItem item) async {
-    User? user = await _firebaseService.getUser(item.owner);
+Future<void> _showItemDialog(BuildContext context, MaterialItem item) async {
+  User? user = await _firebaseService.getUser(item.owner);
 
-    return showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          backgroundColor: Colors.white,
-          title: Text(item.title),
-          content: SingleChildScrollView(
-            child: SizedBox(
-              width: double.maxFinite,
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  if (item.images.isNotEmpty && item.images.first.startsWith('http'))
-                    CachedNetworkImage(
-                      imageUrl: item.images.first,
-                      width: double.infinity,
-                      fit: BoxFit.cover,
-                      memCacheHeight: 800,
-                      memCacheWidth: 600,
-                      errorWidget: (context, error, stackTrace) {
-                        return Image.asset('assets/images/$rand.jpg');
-                      },
-                    )
-                  else
-                    Image.asset(
-                      item.images.first,
-                      width: double.infinity,
-                      fit: BoxFit.cover,
+  return showDialog(
+    context: context,
+    builder: (context) {
+      return AlertDialog(
+        backgroundColor: Colors.white,
+        title: Text(item.title),
+        content: SingleChildScrollView(
+          child: SizedBox(
+            width: double.maxFinite,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Card(
+                  elevation: 4.0,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        if (item.images.isNotEmpty && item.images.first.startsWith('http'))
+                          CachedNetworkImage(
+                            imageUrl: item.images.first,
+                            width: double.infinity,
+                            fit: BoxFit.cover,
+                            memCacheHeight: 800,
+                            memCacheWidth: 600,
+                            errorWidget: (context, error, stackTrace) {
+                              return Image.asset('assets/images/$rand.jpg');
+                            },
+                          )
+                        else
+                          Image.asset(
+                            item.images.first,
+                            width: double.infinity,
+                            fit: BoxFit.cover,
+                          ),
+                        const SizedBox(height: 8.0),
+                        Text('Description: ${item.description}'),
+                        Text('Categories: ${item.categories.join(', ')}'),                
+                        Text('Condition: ${item.condition}'),
+                        Text('Interchangeable: ${item.interchangeable}'),
+                        Text('Views: ${item.views}'),
+                        Text('Likes: ${item.likes}'),                  
+                        Text('Price: \$${_formatPrice(item.price)}', style: const TextStyle(fontWeight: FontWeight.bold)),
+                      ],
                     ),
-                  const SizedBox(height: 8.0),
-                  Text('Description: ${item.description}'),
-                  Text('Categories: ${item.categories.join(', ')}'),                
-                  Text('Condition: ${item.condition}'),
-                  Text('Interchangeable: ${item.interchangeable}'),
-                  Text('Views: ${item.views}'),
-                  Text('Likes: ${item.likes}'),                  
-                  Text('Price: \$${_formatPrice(item.price)}', style: const TextStyle(fontWeight: FontWeight.bold)),
-                  if (user != null) ...[
-                    const SizedBox(height: 16.0),
-                    Card(
-                      elevation: 4.0,
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Column(
-                          mainAxisSize: MainAxisSize.min,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            Text('Owner Information', style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold)),
-                            const SizedBox(height: 8.0),
-                            CachedNetworkImage(
-                              imageUrl: user.imageUrl,
+                  ),
+                ),
+                if (user != null) ...[
+                  const SizedBox(height: 16.0),
+                  Card(
+                    elevation: 4.0,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text('Owner Information', style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold)),
+                          const SizedBox(height: 8.0),
+                          CachedNetworkImage(
+                            imageUrl: user.imageUrl,
+                            width: 120,
+                            height: 120,
+                            fit: BoxFit.cover,
+                            imageBuilder: (context, imageProvider) => Container(
                               width: 120,
                               height: 120,
-                              fit: BoxFit.cover,
-                              imageBuilder: (context, imageProvider) => Container(
-                                width: 120,
-                                height: 120,
-                                decoration: BoxDecoration(
-                                  shape: BoxShape.circle,
-                                  image: DecorationImage(
-                                    image: imageProvider,
-                                    fit: BoxFit.cover,
-                                  ),
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                image: DecorationImage(
+                                  image: imageProvider,
+                                  fit: BoxFit.cover,
                                 ),
                               ),
-                              memCacheHeight: 300,
-                              memCacheWidth: 220,
-                              errorWidget: (context, error, stackTrace) {
-                                return const Icon(Icons.person, size: 120);
-                              },
                             ),
-                            const SizedBox(height: 8.0),
-                            Text('Username: ${user.username}'),
-                            Text('Name: ${user.name}'),
-                            Text('Email: ${user.email}'),
-                            Text('Career: ${user.carrer}'),
-                          ],
-                        ),
+                            memCacheHeight: 300,
+                            memCacheWidth: 220,
+                            errorWidget: (context, error, stackTrace) {
+                              return const Icon(Icons.person, size: 120);
+                            },
+                          ),
+                          const SizedBox(height: 8.0),
+                          Text('Username: ${user.username}'),
+                          Text('Name: ${user.name}'),
+                          Text('Email: ${user.email}'),
+                          Text('Career: ${user.carrer}'),
+                        ],
                       ),
                     ),
-                  ],
+                  ),
                 ],
-              ),
+              ],
             ),
           ),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: const Text('Close'),
-            ),
-          ],
-        );
-      },
-    );
-  }
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.of(context). pop();
+            },
+            child: const Text('Close'),
+          ),
+        ],
+      );
+    },
+  );
 }
+}
+
 
 class HeartIconButton extends StatelessWidget {
   final bool isLiked;
